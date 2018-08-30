fc790cdad0a9b23701c14feae2c941e9e5fdaee9
aaron <aaront1144@gmail.com>
2018-08-30 01:52:29 -0400

    ...

--- tableCreationScripts.py
+++ tableCreationScripts.py
@@ -7,30 +7,33 @@ date:8/28/18
 import sqlite3
 import sys
 
-connection = sqlite3.connect(sys.argv[1])
-cursor = connection.cursor()
+def main():
+    connection = sqlite3.connect(sys.argv[1])
+    cursor = connection.cursor()
 
-sql_createMachine = '''
-CREATE TABLE machine (
-ID int NOT NULL PRIMARY KEY,
-MAC_address VARCHAR(255) NOT NULL,
-IP_address VARCHAR(255) NOT NULL,
-last_updated DATETIME,
-CONSTRAINT fields_unique (MAC_address, IP_address),
-FOREIGN KEY(ID) REFERENCES hardware(ID))
-'''
+    sql_createMachine = '''
+    CREATE TABLE machine (
+    ID int NOT NULL PRIMARY KEY,
+    MAC_address VARCHAR(255) NOT NULL,
+    IP_address VARCHAR(255) NOT NULL,
+    last_updated DATETIME,
+    CONSTRAINT fields_unique (MAC_address, IP_address),
+    FOREIGN KEY(ID) REFERENCES hardware(ID))
+    '''
 
-sql_createHardware = """
-CREATE TABLE hardware (
-ID int NOT NULL PRIMARY KEY,
-card_type VARCHAR(30),
-core_clock int,
-mem_clock int,
-cards_active int,
-hashrate REAL);
-"""
+    sql_createHardware = """
+    CREATE TABLE hardware (
+    ID int NOT NULL PRIMARY KEY,
+    card_type VARCHAR(30),
+    core_clock int,
+    mem_clock int,
+    cards_active int,
+    hashrate REAL)
+    """
 
-cursor.execute(sql_createMachine)
-cursor.execute(sql_createHardware)
+    cursor.execute(sql_createMachine)
+    cursor.execute(sql_createHardware)
+    cursor.commit()
 
-cursor.close()
+    cursor.close()
+main()
