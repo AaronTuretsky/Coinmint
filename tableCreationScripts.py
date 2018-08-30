ac2aae0dd7e8856c2424c060b64bdbb93970e700
aaron <aaront1144@gmail.com>
2018-08-30 01:35:06 -0400

    ...

--- tableCreationScripts.py
+++ tableCreationScripts.py
@@ -10,7 +10,15 @@ import sys
 connection = sqlite3.connect(sys.argv[1])
 cursor = connection.cursor()
 
-sql_createMachine = "ddddd"
+sql_createMachine = '''
+CREATE TABLE machine (
+ID int NOT NULL PRIMARY KEY,
+MAC_address VARCHAR(255) NOT NULL,
+IP_address VARCHAR(255) NOT NULL,
+last_updated DATETIME,
+CONSTRAINT fields_unique (MAC_address, IP_address),
+FOREIGN KEY(ID) REFERENCES hardware(ID));
+'''
 
 sql_createHardware = """
 CREATE TABLE hardware (
@@ -22,7 +30,15 @@ cards_active int,
 hashrate REAL);
 """
 
-cursor.execute(sql_createMachine)
+cursor.execute('''
+CREATE TABLE machine (
+ID int NOT NULL PRIMARY KEY,
+MAC_address VARCHAR(255) NOT NULL,
+IP_address VARCHAR(255) NOT NULL,
+last_updated DATETIME,
+CONSTRAINT fields_unique (MAC_address, IP_address),
+FOREIGN KEY(ID) REFERENCES hardware(ID));
+''')
 cursor.execute(sql_createHardware)
 
 cursor.close()
